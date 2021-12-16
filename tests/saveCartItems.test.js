f04b9bd167c5d03337176a38bf6dee5e4283e605
Matheus Alves <matheus.aalves@hotmail.com>
2021-12-16 20:06:10 -0300

    Testando

--- tests/saveCartItems.test.js
+++ tests/saveCartItems.test.js
@@ -9,7 +9,7 @@ describe('4 - Teste a função saveCartItems', () => {
     saveCartItems(arg);
     expect(localStorage.setItem).toHaveBeenCalled();
   });
-  it('se, ao executar saveCartItems com o argumento <ol><li>Item</li></ol>, o método localStorage.setItem é chamado com dois parâmetros, sendo o primeiro "cartItems" e o segundo sendo o valor passado como argumento para saveCartItems', () => {
+  it('se ao executar saveCartItems com o argumento <ol><li>Item</li></ol>, o método localStorage.setItem é chamado com dois parâmetros, sendo o primeiro "cartItems" e o segundo sendo o valor passado como argumento para saveCartItems', () => {
    const arg = document.querySelector('.cart__items li');
    saveCartItems(arg);
    expect(localStorage.setItem).toHaveBeenCalledWith('cartItems', arg);
